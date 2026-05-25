b6cec101611b524dce733cd544dafc82a0983330
ben zhong <97631450+benzhongg@users.noreply.github.com>
2026-05-25 08:33:21 -0400

    Impl stage 2.5 - Small test in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <hpringbuffer.h>
+#include <list>
 
 int main(){
+
+    HighPerformanceRingBuffer<int, 5> highperformance1;
+    std::cout << highperformance1.capacity();
+
+    highperformance1.insert(1);
+    std::cout << highperformance1.size();
+
+    highperformance1.insert({1, 2});
+    std::cout << highperformance1.size();
+    
+    //buffer is [1, 1, 2...]
+    std::cout << highperformance1.get();
+    std::list<int> multi_get_result = highperformance1.get(2);
+    for (auto index : multi_get_result){
+        std::cout << index << " ";
+    }
+    
+    // highperformance1.remove(1);
+    // std::cout << highperformance1.size();
+    // std::cout << highperformance1.get();
+
+    // highperformance1.clear_buffer();
+    
 }
