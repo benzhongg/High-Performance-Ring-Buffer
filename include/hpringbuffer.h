eede288921b0aa5be38733529ca98b7b2240529d
ben zhong <97631450+benzhongg@users.noreply.github.com>
2026-05-29 02:10:55 -0400

    Feat(atomics): Changing member vars and methods
    - head & tail -> std::atomics
    - methods -> follow correct memory orders (draft #1)
    - notes on atomics in comments

--- include/hpringbuffer.h
+++ include/hpringbuffer.h
@@ -13,11 +13,18 @@ class HighPerformanceRingBuffer
 private:
     std::array<T, CAPACITY> m_buffer { default_value() };
     
+
+    // thing to note
+        // False Sharing -> if head and tail are atomics and placed next to each other in mem
+        // They may end up on the same cache line, meaning either one of them might invalidate
+            // the other for trying to read/update
+            // this is solved through "padding" like a char padding[64];
+            
     //head is next write
-    int m_head { 0 };
+    std::atomic<int> m_head { 0 };
     
     //tail is next read
-    int m_tail { 0 };
+    std::atomic<int> m_tail { 0 };
 
 protected:
     T default_value()
@@ -42,13 +49,27 @@ public:
     ~HighPerformanceRingBuffer() = default;
 
     bool push(T v)
+    //are either atomics affected here?
+    //yes
     { 
         if (!isFull())
         {
-            m_buffer[m_head] = v;
-
-            m_head = (m_head + 1) % CAPACITY;
+            // acquire pertains to loads any further load can't be before this one or any behind
+            // release pertains to storing past or any following 
             
+            // consume a specific version of acquire
+            // relaxed -> all memory reorderings are ok
+
+            // in this case we'll be choosing acq rel
+
+            // memory_order_acq_rel is for RMW events/operations
+            int tempHead = m_head.load(std::memory_order_acquire);
+
+            m_buffer[tempHead] = v;
+
+            tempHead = (tempHead + 1) % CAPACITY;
+            
+            m_head.store(tempHead, std::memory_order_release);
             return true;
         } 
         else 
@@ -79,9 +100,13 @@ public:
             return default_value();
         }
         
-        T res = m_buffer[m_tail];
-        m_tail = (m_tail + 1) % CAPACITY;
+        int tempTail = m_tail.load(std::memory_order_acquire);
         
+        T res = m_buffer[tempTail];
+        tempTail = (tempTail + 1) % CAPACITY;
+        
+        m_tail.store(tempTail, std::memory_order_release);
+
         return res;
     }
     
@@ -109,22 +134,33 @@ public:
     //returns element count
     size_t size() const
     {
-        if (m_tail < m_head){
-            return m_head - m_tail;
+
+        // there is a potential error here -> even though they are safeguarded individually, what
+        // happens when tail is head is read, head gets affected via a push(), and tail is loaded?
+            // we'd be calculating a size that is no longer valid
+        int temp_head = m_head.load(std::memory_order_acquire);
+        int temp_tail = m_tail.load(std::memory_order_acquire);
+
+        if (temp_tail < temp_head){
+            return temp_head - temp_tail;
         }
         
-        if (m_head < m_tail){
-            return CAPACITY - m_tail - m_head;
+        if (temp_head < temp_tail){
+            return CAPACITY - temp_tail - temp_head;
         }
         
         //H == T when empty, our FULL case won't occur -> see isFull()
         return 0;
     }
     
+    // these are both stores -> debating between .store or .exchange
+    // .store is the straightforward release after setting to 0 (faster)
+    // .exchange is the acq (sync) with all threads then setting to 0 (slower, more hardware, but performant if
+        // knowledge of the past is required)
     void clearBuffer()
     {
-        m_tail = 0;
-        m_head = 0;
+        m_tail.store(0, std::memory_order_release);
+        m_head.store(0, std::memory_order_release);
     }
     
     size_t capacity()
